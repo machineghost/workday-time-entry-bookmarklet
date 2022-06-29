e04cb79257cffce5a654602d7cf93e6c65776b69
Jeremy Walker <machineghost@gmail.com>
2022-06-29 16:32:03 -0700

    Updated page and added video

---
+++ bookmarkletDetails.js
@@ -0,0 +1,45 @@
+// Group
+const group = {
+  sessionLength: 60,
+  dropdownValues: [
+    ['Format', 'Office Hours'],
+    ['Discipline', 'Web Dev'],
+    ['Course Type', 'N/A'],
+  ],
+};
+
+// Immersion (30)
+const immersion30 = {
+  sessionLength: 40,
+  dropdownValues: [
+    ['Format', '1:1/Mentorship'],
+    ['Discipline', 'Web Dev'],
+    ['Course Type', 'Immersion'],
+  ],
+};
+
+// Immersion (45)
+const immersion45 = {
+  sessionLength: 60,
+  dropdownValues: [
+    ['Format', '1:1/Mentorship'],
+    ['Discipline', 'Web Dev'],
+    ['Course Type', 'Immersion'],
+  ],
+};
+
+// Flex
+const flex = {
+  sessionLength: 40,
+  dropdownValues: [
+    ['Format', '1:1/Mentorship'],
+    ['Discipline', 'Web Dev'],
+    ['Course Type', 'Flex'],
+  ],
+};
+
+// HQ
+const hq = {
+    sessionLength: 30,
+    [    ['Format', 'HQ'],    ['Discipline', 'Web Dev'],    ['Course Type', 'N/A'],  ]
+}
